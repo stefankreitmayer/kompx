0ac37afd9a5d10a7550dd3696a0f9eea2d6f28d7
stefankreitmayer <stefankreitmayer@gmail.com>
2016-08-31 15:11:28 +0200

    bugfix: order of sections in json

--- test/controllers/frame_controller_test.exs
+++ test/controllers/frame_controller_test.exs
@@ -5,7 +5,7 @@ defmodule Kompax.FrameControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  test "shows the frame", %{conn: conn} do
+  test "shows the frame, comprising activities and aspects", %{conn: conn} do
     conn = get conn, frame_path(conn, :show)
     assert json_response(conn, 200) == %{"frame" => %{"activities" => [], "aspects" => []}}
   end
--- web/controllers/frame_controller.ex
+++ web/controllers/frame_controller.ex
@@ -2,11 +2,14 @@ defmodule Kompax.FrameController do
   use Kompax.Web, :controller
 
   alias Kompax.Activity
+  alias Kompax.Section
   alias Kompax.Aspect
   alias Kompax.Tag
 
   def show(conn, _params) do
-    activities = Repo.all(from a in Activity, where: a.published) |> Repo.preload([:sections, :annotations])
+    activities = Repo.all(from a in Activity, where: a.published)
+              |> Repo.preload(sections: (from s in Section, order_by: [asc: s.position]))
+              |> Repo.preload(:annotations)
     aspects = Repo.all(Aspect)
               |> Repo.preload(tags: (from t in Tag, order_by: [asc: t.name]))
     render(conn, "show.json", frame: %{activities: activities, aspects: aspects})
